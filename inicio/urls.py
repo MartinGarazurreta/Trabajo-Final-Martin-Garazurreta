184b2f02b55021facde8c30c7daff37a84c4e3a2
martin <garazurretamartin@gmail.com>
2023-10-31 12:12:20 -0300

    inicializacion del proyecto

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from inicio.views import inicio, autos, crear_auto
+from inicio.views import inicio, autos, crear_auto, Camion, crear_camion, Vans, crear_vans
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('autos/', autos, name='autos'),
     path('autos/crear/', crear_auto, name='crear_auto'),
+    path('camion/', Camion, name='camion'),
+    path('camion/crear/', crear_camion, name='crear_camion'),
+    path('vans/', Vans, name='vans'),
+    path('vans/crear/', crear_vans, name='crear_vans'),
 ]
