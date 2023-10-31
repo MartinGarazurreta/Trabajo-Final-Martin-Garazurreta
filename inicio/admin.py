b2eee8089999bd4b4b8490c529941b8642970439
martin <garazurretamartin@gmail.com>
2023-10-31 12:16:58 -0300

    inicializacion del proyecto

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from inicio.models import Auto
+from inicio.models import Auto, Camion, Vans
 
 admin.site.register(Auto)
+admin.site.register(Camion)
+admin.site.register(Vans)
 # Register your models here.
